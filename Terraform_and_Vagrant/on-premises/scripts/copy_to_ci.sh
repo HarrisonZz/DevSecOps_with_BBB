f34a77b698273c01f30c9a34ea38fd9a04f77826
Harrison <harrison_0418@outlook.com>
2025-10-27 03:37:24 +0800

    Deal with permission and format issue

--- Terraform_and_Vagrant/on-premises/scripts/copy_to_ci.sh
+++ Terraform_and_Vagrant/on-premises/scripts/copy_to_ci.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-# scripts/copy_to_ci.sh
 
 PROJECT_DIR="$(pwd)"
 JENKINS_HOME="/var/lib/jenkins"
 
-mkdir -p "$JENKINS_HOME/.ssh"
-cp "$PROJECT_DIR/.ssh_config" "$JENKINS_HOME/.ssh/vagrant_config"
+sudo cp "$PROJECT_DIR/.ssh_config" "$JENKINS_HOME/.ssh/vagrant_config"
 chmod 600 "$JENKINS_HOME/.ssh/vagrant_config"
 
 for vm in "$@"; do
   KEY_SRC="$PROJECT_DIR/.vagrant/machines/$vm/vmware_desktop/private_key"
   KEY_DEST="$JENKINS_HOME/.ssh/${vm}_key"
 
-  cp "$KEY_SRC" "$KEY_DEST"
-  chmod 600 "$KEY_DEST"
+  sudo cp "$KEY_SRC" "$KEY_DEST"
+  sudo chmod 600 "$KEY_DEST"
 done
 
-chown -R jenkins:jenkins "$SSH_DIR"
+sudo chown -R jenkins:jenkins "$SSH_DIR"
