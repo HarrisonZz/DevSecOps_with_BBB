7ab3dda6f8ab50c44ef581d6a70a68a4d921bc6e
Harrison <harrison_0418@outlook.com>
2025-10-27 04:23:20 +0800

    fix error

--- Terraform_and_Vagrant/on-premises/scripts/copy_to_jenkins.sh
+++ Terraform_and_Vagrant/on-premises/scripts/copy_to_jenkins.sh
@@ -3,9 +3,10 @@
 
 PROJECT_DIR="$(pwd)"
 JENKINS_HOME="/var/lib/jenkins"
+SSH_DIR="$JENKINS_HOME/.ssh"
 
 sudo cp "$PROJECT_DIR/.ssh_config" $JENKINS_HOME/.ssh/vagrant_config
-sudo chmod 600 ~/.ssh/vagrant_config
+sudo chmod 600 $JENKINS_HOME/.ssh/vagrant_config
 
 for vm in "$@"; do
   KEY_SRC="$PROJECT_DIR/.vagrant/machines/$vm/vmware_desktop/private_key"
