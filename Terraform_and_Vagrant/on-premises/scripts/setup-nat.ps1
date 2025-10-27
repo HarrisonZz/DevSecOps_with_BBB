10e75f1ecfd363111a47ccc3f1e2649bab2414db
Harrison <harrison_0418@outlook.com>
2025-10-28 01:52:20 +0800

    Modify ansible playbook and local Terraform file

--- Terraform_and_Vagrant/on-premises/scripts/setup-nat.ps1
+++ Terraform_and_Vagrant/on-premises/scripts/setup-nat.ps1
@@ -1,7 +1,7 @@
 # 檢查是否已存在 NetNat
-if (!(Get-NetNat -Name 'USB_NAT' -ErrorAction SilentlyContinue)) {
-    Write-Host "Creating USB_NAT NAT..."
-    New-NetNat -Name 'USB_NAT' -InternalIPInterfaceAddressPrefix 192.168.137.0/24
+if (!(Get-NetNat -Name 'BBB_NAT' -ErrorAction SilentlyContinue)) {
+    Write-Host "Creating BBB_NAT NAT..."
+    New-NetNat -Name 'BBB_NAT' -InternalIPInterfaceAddressPrefix 192.168.137.0/24
 } else {
-    Write-Host "NetNat USB_NAT already exists."
+    Write-Host "NetNat BBB_NAT already exists."
 }
